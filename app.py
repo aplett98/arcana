efe6180db6e0d4c16526b0ec0a7e0f7c4b31bf0d
Gunnar Plunkett <glplunkett@college.harvard.edu>
2020-07-31 16:37:36 -0700

    added turn functionality

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ sio = socketio.AsyncServer()
 app = web.Application()
 sio.attach(app)
 game = gameobjects.Game()
-sid_to_num = {}
+sid_to_pid = {}
 player_sids = []
 
 
@@ -24,22 +24,22 @@ def decode_target(data):
     elif 'player' not in data['target']:
         raise MsgError("No target player specified.")
     elif 'stack' in data['target'] and 'card' in data['target']:
-        target_player = data['target']['player']
+        target_player = game.players[data['target']['player']]
         softened_suit = Suit(data['target']['card'][0])
         softened_pos = data['target']['card'][1]
-        softened_card = target_player.stacks[softened_suit][softened_pos]
+        softened_card = target_player.stacks[softened_suit].cards[softened_pos]
         target_suit = data['target']['stack']
         target_stack = target_player.stacks[target_suit]
         target = softened_card, target_stack
     elif 'stack' in data['target']:
-        target_player = data['target']['player']
+        target_player = game.players[data['target']['player']]
         suit = Suit(data['target']['stack'])
         target = target_player.stacks[suit]
     elif 'card' in data['target']:
-        target_player = data['target']['player']
+        target_player = game.players[data['target']['player']]
         suit = Suit(data['target']['card'][0])
         pos = data['target']['card'][1]
-        target = target_player.stacks[suit][pos]
+        target = target_player.stacks[suit].cards[pos]
     else:
         raise MsgError("Invalid move target.")
     return target, target_player
@@ -72,6 +72,19 @@ def decode_move(data, player=None):
     return move, source, target, player, target_player
 
 
+async def state_change():
+    win_state = game.check_win_condition()
+    if win_state is not None:
+        print("Game Over.")
+        await sio.emit('winner', win_state)
+    else:
+        for sid, pid in sid_to_pid.items():
+            await sio.emit(
+                'state_change',
+                game.get_state(pid), room=sid
+            )
+
+
 async def index(request):
     return web.Response(text=pformat(game.get_state()), content_type='text')
 
@@ -83,10 +96,10 @@ async def connect(sid, environ):
     print("Connecting...")
     id = game.add_player()
     print("Connected Player %d." % (id+1))
-    sid_to_num[sid] = id
+    sid_to_pid[sid] = id
     player_sids.append(sid)
-    sio.emit('joined', id, room=sid)
-    sio.emit('state_change', game.get_state())
+    await sio.emit('joined', id, room=sid)
+    await state_change()
 
 
 @sio.event
@@ -94,49 +107,49 @@ async def start_game(sid):
     print("Starting game...")
     game.start()
     print("Game started.")
-    sio.emit('state_change', game.get_state())
+    await state_change()
 
 
 @sio.event
 async def try_move(sid, data):
-    player = game.players[sid_to_num[sid]]
+    player = game.players[sid_to_pid[sid]]
     try:
         move, source, target, _, target_player = decode_move(
             data, player=player)
     except MsgError as err:
-        sio.emit('msg_error', err.message, room=sid)
+        await sio.emit('msg_error', err.message, room=sid)
         return
     if game.move_is_challengeable(move, source, target):
         data['source']['player'] = player.id
         room = None if target_player is None else player_sids[target_player.id]
-        sio.emit('offer_challenge', data, room=room)
+        await sio.emit('offer_challenge', data, room=room)
     else:
         try:
             game.handle_move(move, source, target)
         except GameError as err:
             room = None if err.player is None else player_sids[err.player]
-            sio.emit('game_error', err.message, room=room)
+            await sio.emit('game_error', err.message, room=room)
         else:
-            sio.emit('state_change', game.get_state())
+            await state_change()
 
 
 @sio.event
 async def challenge(sid, data):
-    target_player = game.players[sid_to_num[sid]]
+    target_player = game.players[sid_to_pid[sid]]
     try:
         move, source, target, player, _ = decode_move(data)
         attacker_won = game.handle_challenge(move, source, target)
         if attacker_won:
             game.handle_move(move, source, target)
-            sio.emit('state_change', game.get_state())
+            await state_change()
     except MsgError as err:
-        sio.emit('msg_error', err.message, room=sid)
+        await sio.emit('msg_error', err.message, room=sid)
         return
     except GameError as err:
         room = None if err.player is None else player_sids[err.player]
-        sio.emit('game_error', err.message, room=room)
+        await sio.emit('game_error', err.message, room=room)
         return
-    sio.emit('challenge_results', {
+    await sio.emit('challenge_results', {
         'winner': player.id if attacker_won else target_player.id,
         'loser': target_player.id if attacker_won else player.id
     })
@@ -144,7 +157,7 @@ async def challenge(sid, data):
 
 @sio.event
 async def punish(sid, data):
-    player = game.players[sid_to_num[sid]]
+    player = game.players[sid_to_pid[sid]]
     try:
         if 'move' in data:
             move = data['move']
@@ -153,23 +166,12 @@ async def punish(sid, data):
         target, _ = decode_target(data)
         game.handle_punish(player, move, target)
     except MsgError as err:
-        sio.emit('msg_error', err.message, room=sid)
+        await sio.emit('msg_error', err.message, room=sid)
     except GameError as err:
         room = None if err.player is None else player_sids[err.player]
-        sio.emit('game_error', err.message, room=room)
+        await sio.emit('game_error', err.message, room=room)
     else:
-        sio.emit('state_change', game.get_state)
-
-
-@sio.event
-async def get_nplayers(sid):
-    print(sid)
-    print("There are %d players." % game.nplayers())
-    async with sio.session(sid) as session:
-        if 'id' in session:
-            print("You are Player %d." % (session['id']+1))
-        else:
-            print("You are not in the game.")
+        await sio.emit('state_change', game.get_state)
 
 
 if __name__ == '__main__':
--- gameobjects.py
+++ gameobjects.py
@@ -8,14 +8,6 @@ class Suit(IntEnum):
     CONVINCER = 2
     WARRIOR = 3
 
-    def __str__(self):
-        return {
-            Suit.SPY: 'spy',
-            Suit.WIZARD: 'wizard',
-            Suit.CONVINCER: 'convincer',
-            Suit.WARRIOR: 'warrior'
-        }[self]
-
     def to_card(self):
         return {
             Suit.SPY: SpyCard,
@@ -32,6 +24,14 @@ class Suit(IntEnum):
             Suit.WARRIOR: WarriorStack
         }[self]
 
+    def moves(self):
+        return {
+            Suit.SPY: {Move.SPY, Move.CAPTURE},
+            Suit.WIZARD: {Move.CROSS, Move.SOFTEN},
+            Suit.CONVINCER: {Move.CONVINCE, Move.CURE},
+            Suit.WARRIOR: {Move.ATTACK, Move.MARTYR}
+        }[self]
+
 
 class Move(IntEnum):
     SPY = 0
@@ -42,6 +42,26 @@ class Move(IntEnum):
     CURE = 5
     ATTACK = 6
     MARTYR = 7
+
+    def suit(self):
+        return {
+            move: suit for suit in Suit for move in suit.moves()
+        }[self]
+
+    def from_card(self):
+        return self in {Move.CAPTURE, Move.CURE}
+
+    def execute(self):
+        return {
+                Move.SPY: SpyStack.spy,
+                Move.CAPTURE: SpyCard.capture,
+                Move.CROSS: WizardStack.cross,
+                Move.SOFTEN: WizardStack.soften,
+                Move.CONVINCE: ConvincerStack.convince,
+                Move.CURE: ConvincerCard.cure,
+                Move.ATTACK: WarriorStack.attack,
+                Move.MARTYR: WarriorStack.martyr,
+            }[self]
 
 
 class GameError(Exception):
@@ -80,8 +100,12 @@ class Game(object):
 
     def next_turn(self):
         if self.started:
-            self.turn = self.turn + 1 if self.turn < self.nplayers() else 0
-            self.start_turn()
+            self.turn = self.turn + 1 if self.turn < self.nplayers()-1 else 0
+            if self.players[self.turn].eliminated():
+                # if player is eliminated, skip them
+                self.next_turn()
+            else:
+                self.start_turn()
 
     def martyr(self, suit):
         for player in self.players:
@@ -91,26 +115,96 @@ class Game(object):
         return len(self.players)
 
     def move_is_challengeable(self, move, source, target):
-        # TODO
-        pass
+        """
+            Returns True if the given move is challengeable,
+            and False otherwise.
+        """
+        if move is Move.SPY:
+            return False
+        elif move is Move.MARTYR:
+            return source.min_known_strength() < 10
+        elif move is Move.CONVINCE:
+            softened = target[0]
+            return source.min_known_strength <= softened.min_known_strength()
+        else:
+            return source.min_known_strength() <= target.min_known_strength()
 
     def handle_move(self, move, source, target):
-        # TODO
-        pass
+        """Update game state according to the move."""
+        if move is Move.CONVINCE and type(target) is tuple:
+            # target should be a tuple of softened, target_stack
+            target = target[1]
+        elif move is Move.CONVINCE or type(target) is tuple:
+            raise GameError(
+                "Invalid target.", player=source.owner.id)
+        try:
+            make_move = move.execute()
+            make_move(source, target)
+        except AttributeError:
+            raise GameError(
+                "This move is illegal.", player=source.owner.id)
 
     def handle_challenge(self, move, source, target):
-        # TODO
-        pass
+        """
+            Returns True if the attacker (source) won the challenge,
+            and False otherwise.
+            If the attacker won, execute the move.
+        """
+        if move is Move.CONVINCE and type(target) is tuple:
+            # target should be a tuple of softened, target_stack
+            softened = target[0]
+            source.show()
+            softened.show()
+            if source.strength() <= softened.strength():
+                return False
+        elif move is Move.CONVINCE or type(target) is tuple:
+            raise GameError(
+                "Invalid target.", player=source.owner.id)
+        elif move is move.SPY:
+            raise GameError("Spying cannot be challenged.")
+        elif move is move.MARTYR:
+            source.show()
+            if source.strength() < 10:
+                return False
+        else:
+            source.show()
+            target.show()
+            if source.strength() <= target.strength():
+                return False
+        # at this point, the attacker has won the challenge.
+        self.handle_move(move, source, target)
+        return True
 
-    def handle_punish(self, move, source, target):
-        # TODO
-        pass
+    def handle_punish(self, player, move, target):
+        suit = move.suit()
+        # dummy card with infinite strength
+        captain_card = suit.to_card()(100, -1, self)
+        if move.from_card():
+            captain = captain_card
+            captain.owner = player
+        else:
+            # dummy stack with infinite strength
+            captain = suit.to_stack()(player).add_card(captain_card)
+        self.handle_move(move, captain, target)
 
-    def get_state(self):
+    def check_win_condition(self):
+        """
+        If a player meets the win condition, return their pid.
+        Otherwise, return None.
+        """
+        remaining = [
+            player for player in self.players if not player.eliminated()
+        ]
+        return remaining[0] if len(remaining) == 1 else None
+
+    def get_state(self, pid=None):
         state = {
             'started': self.started,
             'deck': self.deck.get_state(),
-            'players': [player.get_state() for player in self.players],
+            'players': [
+                player.get_state(private=(id == pid))
+                for id, player in enumerate(self.players)
+            ],
         }
         if self.started:
             state = {**state, **{
@@ -132,10 +226,14 @@ class Player(object):
         new_card = self.game.deck.pop_card()
         self.add_card(new_card)
 
-    def get_state(self):
+    def eliminated(self):
+        return all(stack.is_empty() for stack in self.stacks.values())
+
+    def get_state(self, private=False):
         return {
-            str(suit):
-                stack.get_state() for (suit, stack) in self.stacks.items()
+            suit.value:
+                stack.get_state(private=private)
+                for (suit, stack) in self.stacks.items()
             }
 
 
@@ -154,8 +252,7 @@ class Deck(object):
 
     def pop_card(self):
         if self.size() > 1:
-            card = self.cards.pop()
-            return card
+            return self.cards.pop()
 
     def deal(self, nplayers):
         spies = [c for c in self.cards if c.suit is Suit.SPY]
@@ -182,37 +279,60 @@ class Deck(object):
         return {'size': self.size()}
 
 
-class Card(object):
+class Suited():
+    """Superclass for cards and stacks."""
+    def __init__(self, suit, owner=None):
+        self.suit = suit
+        self.owner = owner
+
+    def strength(self) -> int:
+        raise NotImplementedError("Implement this function.")
+
+    def min_known_strength(self) -> int:
+        raise NotImplementedError("Implement this function.")
+
+    def show(self):
+        raise NotImplementedError("Implement this function.")
+
+
+class Card(Suited):
     '''The general class for cards in the game.'''
-    def __init__(self, strength, suit, id, game):
+    def __init__(self, value, suit, id, game):
+        Suited.__init__(self, suit)
         self.id = id
         self.game = game
-        self.strength = strength
-        self.suit = suit
+        self.value = value
         self.stack = None
         self.shown = False
-        self.owner = None
         self.softened = False
 
     def pop_from_stack(self):
         return self.stack.pop_card(self)
 
-    def strength(self):
-        return self.strength
+    def min_known_strength(self):
+        return self.strength() if self.shown else 1
 
-    def get_state(self):
+    def strength(self):
+        return self.value
+
+    def show(self):
+        self.shown = True
+
+    def get_state(self, private=True):
         return {
-            'strength': self.strength,
+            'strength':
+                self.strength() if private or self.shown
+                else self.min_known_strength(),
             'softened': self.softened,
             'shown': self.shown,
         }
 
 
-class Stack(object):
+class Stack(Suited):
     '''The general class for stacks of a single suit.'''
     def __init__(self, suit, owner):
-        self.owner = owner
-        self.suit = suit
+        Suited.__init__(self, suit, owner)
+        self.game = self.owner.game
         self.cards = []
 
     def highest(self, n=None):
@@ -227,11 +347,14 @@ class Stack(object):
         elif n > 0:
             return reversed(self.cards[-int(n):])
 
+    def min_known_strength(self):
+        return sum(card.min_known_strength() for card in self.cards)
+
     def strength(self):
-        return sum([card.strength for card in self.cards])
+        return sum(card.strength() for card in self.cards)
 
     def sort(self):
-        self.cards.sort()
+        self.cards.sort(key=Card.strength)
         self.cards.reverse()
 
     def add_card(self, card):
@@ -239,7 +362,6 @@ class Stack(object):
         card.owner = self.owner
         self.cards.append(card)
         self.sort()
-        self.size += 1
 
     def pop_highest(self):
         return self.cards.pop(0)
@@ -269,10 +391,16 @@ class Stack(object):
     def is_empty(self):
         return not self.cards
 
-    def get_state(self):
+    def show(self):
+        for card in self.cards:
+            card.show()
+
+    def get_state(self, private=False):
         return {
-            'strength': self.strength(),
-            'cards': [card.get_state() for card in self.cards]
+            'strength':
+                self.strength() if private
+                else self.min_known_strength(),
+            'cards': [card.get_state(private=private) for card in self.cards]
         }
 
 
@@ -284,6 +412,7 @@ class SpyCard(Card):
         new_card = target_card.pop_from_stack()
         self.owner.add_card(new_card)
         self.pop_from_stack()
+        self.game.next_turn()
 
 
 class SpyStack(Stack):
@@ -291,7 +420,8 @@ class SpyStack(Stack):
         Stack.__init__(self, Suit.SPY, owner)
 
     def spy(self, target_card):
-        target_card.shown = True
+        target_card.show()
+        self.game.next_turn()
 
 
 class WizardCard(Card):
@@ -310,6 +440,7 @@ class WizardStack(Stack):
 
     def soften(self, target_card):
         target_card.softened = True
+        target_card.shown = True
 
 
 class ConvincerCard(Card):
